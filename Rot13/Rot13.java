8074edd82d5d9d4d8e159b8eff75a03be6cd8e04
Snr1s3 <m1ss.sunrse@gmail.com>
2024-09-18 20:56:02 +0200

    update

--- Rot13/Rot13.java
+++ Rot13/Rot13.java
@@ -1,69 +1,70 @@
 public class Rot13
 {
-    public final static char[] array_char = 
+    public final static char[] arrayChar = 
     {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
         'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
     };
-    public final static char[] array_char_m = 
+    public final static char[] arrayCharUpper = 
     {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
         'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
     };
 
-    public static char swap_char(char c, int type)
+    public static char SwapChar(char c, int type)
     {
-        for(int i = 0; i < array_char_m.length; i++)
+        for(int i = 0; i < arrayCharUpper.length; i++)
         {
             if(type == 1)
             {
-                if(c == array_char_m[i])
+                if(c == arrayCharUpper[i])
                 {
-                    c = array_char_m[ i+ 13];
+                    c = arrayCharUpper[ i+ 13];
                     return (c);
                 }
             }
             else if(type == 2)
             {
-                if(c == array_char_m[i])
+                if(c == arrayCharUpper[i])
                 {
-                    c = array_char_m[i+ 13];
+                    c = arrayCharUpper[i- 13];
                     return (c);
                 }
             }
             else if(type == 3)
             {
-                if(c == array_char[i])
+                if(c == arrayChar[i])
                 {
-                    c = array_char[i+ 13];
+                    c = arrayChar[i - 13];
                     return (c);
                 }
             }
             else
             {
-                if(c == array_char[i])
+                if(c == arrayChar[i])
                 {
-                    c = array_char[i + 13];
+                    c = arrayChar[i + 13];
                     return (c);
                 }
             }
         }
         return (c);
     }
-    public static String xifraRot13(String message, int rot)
+
+    public static String xifraRot13(String message)
     {
         String newString = "";
         for(int i = 0; i < message.length(); i++)
         {
             if(Character.isUpperCase(message.charAt(i)))
             {
-                newString += swap_char(message.charAt(i), 1);
+                newString += SwapChar(message.charAt(i), 1);
             }
             else
             {
-                newString += swap_char(message.charAt(i), 0);
+                newString += SwapChar(message.charAt(i), 0);
             }
         }
         return (newString);
@@ -75,11 +76,11 @@ public class Rot13
         {
             if(Character.isUpperCase(message.charAt(i)))
             {
-                newString += swap_char(message.charAt(i), 2);
+                newString += swapChar(message.charAt(i), 2);
             }
             else
             {
-                newString += swap_char(message.charAt(i), 3);
+                newString += swapChar(message.charAt(i), 3);
             }
         }
         return (newString);
@@ -87,8 +88,9 @@ public class Rot13
 
     public static void main(String args[])
     {
-        String test = rot13("HoLa BoN DiA", 0)
+        String test = xifraRot13("TbXn ÑbZ PuN");
         System.out.println(test);
-        test = rot13("HoLa BoN DiA", 0)
+        test = desxifraRot13(test);
+        System.out.println(test);
     }
 }
