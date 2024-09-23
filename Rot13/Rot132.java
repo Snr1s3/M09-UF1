8c95e1ccb91ed221f9da348600c0986a713665d5
Snr1s3 <m1ss.sunrse@gmail.com>
2024-09-23 15:37:46 +0200

    petits canvis

--- Rot13/Rot132.java
+++ Rot13/Rot132.java
@@ -1,4 +1,4 @@
-public class Rot13{
+public class Rot132{
     public final static char[] arrayChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     public final static char[] arrayCharUpper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     public static char swapChar(char c, char[] array, boolean bool){
